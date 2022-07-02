5e5fae8efbb5a1f37db9aa2ffa73b36b8cb3f7f4
BlackSystem <anon@anon.com>
2022-07-02 13:07:54 +0200

    corrected routing + working on front design

--- unstable.py
+++ unstable.py
@@ -6,21 +6,27 @@ app = Flask(__name__)
 def front():
     return render_template("front.html")
 
+@app.route("/mixes")
 def mixes():
     return render_template()
 
+@app.route("/block")
 def blog():
     return render_template()
-
+    
+@app.route("/gallery")
 def gallery():
     return render_template()
-
+    
+@app.route("/about")
 def about():
     return render_template()
-
+    
+@app.route("/merch")
 def merch():
     return render_template()
-
+    
+@app.route("/contact")
 def contact():
     return render_template()
  
