553803848a1235e2ba0bd84b55ea87e9909f1dc1
BlackSystem <anon@anon.com>
2022-07-11 12:39:06 +0200

    added function inside unstable.py according to flask docs

--- unstable.py
+++ unstable.py
@@ -1,35 +1,56 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-@app.route("/")
-def front():
-    return render_template("front.html")
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-@app.route("/mixes")
-def mixes():
-    return render_template("mixes.html")
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-@app.route("/block")
-def blog():
-    return render_template()
+    @app.route("/")
+    def front():
+        return render_template("front.html")
+
+    @app.route("/mixes")
+    def mixes():
+        return render_template("mixes.html")
+
+    @app.route("/block")
+    def blog():
+        return render_template()
+        
+    @app.route("/gallery")
+    def gallery():
+        return render_template("gallery.html")
+        
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
+        
+    @app.route("/merch")
+    def merch():
+        return render_template()
+        
+    @app.route("/contact")
+    def contact():
+        return render_template("contact.html")
     
-@app.route("/gallery")
-def gallery():
-    return render_template("gallery.html")
-    
-@app.route("/about")
-def about():
-    return render_template("about.html")
-    
-@app.route("/merch")
-def merch():
-    return render_template()
-    
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
- 
-@app.route("/secret")
-def secret():
-    return render_template("secret.html")
+    @app.route("/secret")
+    def secret():
+        return render_template("secret.html")
+
+    return app
