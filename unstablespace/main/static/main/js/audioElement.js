870f38d93657099084ca4d9a24edf9aa0ab5dae4
nebula <black@nebula>
2022-11-07 14:39:23 +0100

    added sounds and icons

--- unstablespace/main/static/main/js/audioElement.js
+++ unstablespace/main/static/main/js/audioElement.js
@@ -6,10 +6,15 @@ let download = document.querySelector(".download");
 let volume = document.querySelector(".volume");
 let share = document.querySelector(".share");
 let mute = document.querySelector(".mute");
-
-let currentTrack = document.createElement('audio');
+let audio = document.querySelector('.audio');
 
 //test elements will be migrated to a db
+playButton.addEventListener('click', () => {
+    let audio = document.querySelector('.audio');
+    audio.play();
+});
 
-
-
+volume.addEventListener('input', (e) => {
+    const value = e.target.value;
+    audio.volume = value / 100;
+});
