67bb023c6593a3f395ca54dfb0a9c30918a35a2b
nebula <black@nebula>
2022-12-08 11:02:19 +0100

    added volume manipulation

--- unstablespace/main/static/main/js/audioElement.js
+++ unstablespace/main/static/main/js/audioElement.js
@@ -3,16 +3,17 @@ let progressBar = document.querySelector(".progressBar");
 let playbackBar = document.querySelector(".playbackBar");
 let playButtons = document.querySelectorAll(".playButton");
 let download = document.querySelector(".download");
-let volume = document.querySelector(".volume");
+let volumeButtons = document.querySelectorAll(".volume");
 let share = document.querySelector(".share");
 let mute = document.querySelector(".mute");
 let audio = document.querySelectorAll('.audio');
 let ids = document.querySelectorAll('id')
 
 //test elements will be migrated to a db
+
 function playAudio() {
-    audio = this.closest('.rightSide').previousElementSibling.previousElementSibling;
-    audio.play();
+    audioX = this.closest('.rightSide').previousElementSibling.previousElementSibling;
+    audioX.play();
 };
 
 playButtons.forEach(playButton => {
@@ -20,8 +21,11 @@ playButtons.forEach(playButton => {
 });
 
 
-
-volume.addEventListener('input', (e) => {
-    const value = e.target.value;
-    audio.volume = value / 100;
+volumeButtons.forEach(volumeButton => {
+    volumeButton.addEventListener('input', () => {
+    audio = volumeButton.closest('.rightSide').previousElementSibling.previousElementSibling;
+    const volume = volumeButton.value;
+    audio.volume = volume / 100;
+    })
 });
+
